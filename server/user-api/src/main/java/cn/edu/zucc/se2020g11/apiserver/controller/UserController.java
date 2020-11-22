3f87ba9fb63e0ca2d214f30058eb1ecae281ee9f
Tuenity <wantianyu0222@outlook.com>
2020-11-22 14:19:12 +0800

    modify

--- server/user-api/src/main/java/cn/edu/zucc/se2020g11/apiserver/controller/UserController.java
+++ server/user-api/src/main/java/cn/edu/zucc/se2020g11/apiserver/controller/UserController.java
@@ -6,6 +6,7 @@ import cn.edu.zucc.se2020g11.apiserver.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -23,21 +24,25 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("test")
+    @GetMapping("/test")
     public ResponseEntity<UserEntity> test() {
         UserEntity userEntity = new UserEntity();
         userEntity.setUserName("wty");
-        userEntity.setPassword("123");
+        String s = "123";
+        userEntity.setPassword(DigestUtils.md5DigestAsHex(s.getBytes()));
+        System.out.println(userEntity.getPassword());
         return new ResponseEntity<>(userEntity, HttpStatus.OK);
     }
 
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody LoginModel loginModel) {
+        System.out.println(loginModel.getUserName());
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @PostMapping("signup")
+    @PostMapping("/signup")
     public ResponseEntity<UserEntity> signup(@RequestBody UserEntity userEntity) {
         return new ResponseEntity<>(HttpStatus.OK);
     }
+
 }
