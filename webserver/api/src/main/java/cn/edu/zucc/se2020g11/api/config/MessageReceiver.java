7c0f18dd79e447395775cd849b1c644fc949fa53
Tuenity <wantianyu0222@outlook.com>
2020-12-30 10:04:26 +0800

    修改文章状态

--- webserver/api/src/main/java/cn/edu/zucc/se2020g11/api/config/MessageReceiver.java
+++ webserver/api/src/main/java/cn/edu/zucc/se2020g11/api/config/MessageReceiver.java
@@ -1,6 +1,8 @@
 package cn.edu.zucc.se2020g11.api.config;
 
+import cn.edu.zucc.se2020g11.api.entity.CompositionEntity;
 import cn.edu.zucc.se2020g11.api.entity.ErrorEntity;
+import cn.edu.zucc.se2020g11.api.service.CompositionService;
 import cn.edu.zucc.se2020g11.api.service.ErrorService;
 import cn.edu.zucc.se2020g11.api.service.RedisMessageMqService;
 import com.alibaba.fastjson.JSON;
@@ -21,11 +23,13 @@ public class MessageReceiver {
 
     RedisMessageMqService messageMqService;
     ErrorService errorService;
+    CompositionService compositionService;
 
     @Autowired
-    public MessageReceiver(RedisMessageMqService messageMqService, ErrorService errorService) {
+    public MessageReceiver(RedisMessageMqService messageMqService, ErrorService errorService, CompositionService compositionService) {
         this.messageMqService = messageMqService;
         this.errorService = errorService;
+        this.compositionService = compositionService;
     }
 
     /**接收消息的方法*/
@@ -34,19 +38,25 @@ public class MessageReceiver {
         LinkedHashMap linkedHashMap = (LinkedHashMap)messageMqService.pop(resultReceiver);
         String msg = JSON.toJSONString(linkedHashMap);
         System.out.println(msg);
+
+        //取出返回数据
         JSONObject jsonObject = JSONObject.parseObject(msg);
+        int compositionId = jsonObject.getInteger("composition_id");
+        String text = jsonObject.getString("compare_result");
+
+        //更新作文状态
+        CompositionEntity compositionEntity = compositionService.selectCompositionById(compositionId);
+        compositionEntity.setStatus(3);
+        compositionService.updateComposition(compositionEntity, compositionId);
+
+        //语法错误
         ErrorEntity errorEntity = new ErrorEntity();
-        errorEntity.setCompositionId(jsonObject.getInteger("composition_id"));
-        errorEntity.setText(jsonObject.getString("compare_result"));
+        errorEntity.setCompositionId(compositionId);
+        errorEntity.setText(text);
         errorService.addError(errorEntity);
+
         String context = jsonObject.getString("processed_context");
         System.out.println(context);
-//        String context = jsonObject.getString("processed_context");
-//        String regex = ",|，|\\s+";
-//        String[] stringArray = context.split(regex);
-//        for (int i = 0; i < stringArray.length; i++) {
-//            System.out.println("i="+i+" Val="+stringArray[i]);
-//        }
     }
 
 }
